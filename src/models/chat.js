699d93cb5f1e0538d915fecddbce63b09f7137e2
Swann HERERERA <swann.herrera@gmail.com>
2020-03-01 18:25:04 +0100

    clear

--- src/models/chat.js
+++ src/models/chat.js
@@ -1,13 +1,2 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
-
-const conversationSchema = mongoose.Schema({
-  name: {
-    type: String,
-    required: false,
-    trim: true
-  },
-  member: {
-    type: [messageSchema]
-  }
-});
